f3b7c1b6b9494d4bfcfd1917ce5b94d98a069a1f
Maplefin <james.huynh444@gmail.com>
2022-02-10 19:23:21 -0500

    added comments, used all methods

--- AirplaneAssignment/Airplane.java
+++ AirplaneAssignment/Airplane.java
@@ -1,10 +1,16 @@
+/*
+Pradyumn Jha, James Huynh
+Ms. Krasteva ICS4U0 OOP Assignment 2
+Feb 10 2022
+This class simulates an airplane
+*/
 public class Airplane
 {
    private int gasLevel;
    private String landingGear;
    private String doorStatus;
    private int passengers;
-      
+   //constructor for airplane class, takes gas level, landing gear status, door status, and # of passengers
    public Airplane(int gLev, String lGear, String dStat, int p)
    {
       gasLevel = gLev;
@@ -12,77 +18,76 @@ public class Airplane
       doorStatus = dStat;
       passengers = p;
    }
-
+   //This public method sets doorStatus to "Open."
    public void openDoor()
    {
       doorStatus = "Open.";
    }
-
+   //This public method sets doorStatus to "Closed."
    public void closeDoor()
    {
       doorStatus = "Closed.";
    }
-   
+   //This public method sets gasLevel to 100
    public void fillUp()
    {
       gasLevel = 100;
    }
-   
+   //This public method subtracts 30 from gasLevel
    public void takeOff()
    {
       gasLevel -= 30;
    }
-   
+   //This public method sets landingGear to "Up."
    public void doneTakeOff()
    {
       landingGear = "Up.";
    }
-   
+   //This public method subtracts percent from gasLevel
    public void normalFlight(double percent)
    {      
-      gasLevel - percent;     
+      gasLevel -= percent;     
    }
-   
+   //This public method sets landingGear to "Down."
    public void prepLanding()
    {
       landingGear = "Down.";
       
    }
-   
+   //This public method subtracts 15 from gasLevel
    public void land()
    {
       gasLevel -= 15;
    }
-   
+   //This public method adds passengerNum to the amount of passengers.
    public void loadPass(int passengerNum)
    {
-      int numOfPassengers = passengerNum;
-      
-      passengers += numOfPassengers;
+   
+      passengers += passengerNum;
       
    }
-
+   //This public method sets the # of passengers to 0
    public void unloadPass()
    {
       passengers = 0;
       
    }
-   
+   //This public method returns the gas level
    public int getGasLevel()
    {
       return gasLevel;
    }
-   
+   //This public method returns the door's status
    public String getDoorStatus()
    {
       return doorStatus;
    }
-   
+   //This public method returns the # of passengers
    public int getPassengers()
    {
       return passengers;
    }
-   
+   //This public method returns the landing gear's status
    public String getLandingGear()
    {
       return landingGear;
--- AirplaneAssignment/AirplaneDriver.java
+++ AirplaneAssignment/AirplaneDriver.java
@@ -4,11 +4,34 @@ public class AirplaneDriver
 
    public static void main (String[] args)
    {
-      Airplane airplane1 = new Airplane(75, "Down.", "Open.", 850);
+      Airplane airplane1 = new Airplane(20, "Down.", "Open.", 0);
       
-      airplane1.normalFlight(25);
+      System.out.println("Pre-Flight:");
+      System.out.println("The gas level is at " + airplane1.getGasLevel() + " capacity.");
+      System.out.println("The door is " + airplane1.getDoorStatus());
+      System.out.println("There are " + airplane1.getPassengers() + " passengers.");
+      System.out.println("The landing gear is " + airplane1.getLandingGear());
       
+      airplane1.loadPass(850);
+      airplane1.fillUp();
+      airplane1.closeDoor();
+      airplane1.takeOff();
+      airplane1.doneTakeOff();
       
+      System.out.println();
+      System.out.println("During Flight:");
+      System.out.println("The gas level is at " + airplane1.getGasLevel() + " capacity.");
+      System.out.println("The door is " + airplane1.getDoorStatus());
+      System.out.println("There are " + airplane1.getPassengers() + " passengers.");
+      System.out.println("The landing gear is " + airplane1.getLandingGear());
+      
+      airplane1.normalFlight(20);
+      airplane1.prepLanding();
+      airplane1.land();
+      airplane1.unloadPass();
+      
+      System.out.println();
+      System.out.println("In The Hangar: ");
       System.out.println("The gas level is at " + airplane1.getGasLevel() + " capacity.");
       System.out.println("The door is " + airplane1.getDoorStatus());
       System.out.println("There are " + airplane1.getPassengers() + " passengers.");
