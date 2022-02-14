c4c4dd38b65598d4452da3ab3655cb48888d9cf7
James Huynh <81708250+Maplefin444@users.noreply.github.com>
2022-02-14 10:03:26 -0500

    Add files via upload

--- Evaluation2/Windows_Thirteen.java
+++ Evaluation2/Windows_Thirteen.java
@@ -1,23 +1,26 @@
+/*
+James Huynh, Pradyumn Jha
+Ms. Krasteva ICS4U0 Evaluation 2.3
+Feb 14, 2022
+This class inherits the Operating_System class, and simulates a fictional operating system named Windows_Thirteen
+*/
+
 public class Windows_Thirteen extends Operating_System{
    private double currentBal;
+   //Builds the class, uses super() to access the superclass's constructor
    public Windows_Thirteen(long m, String uname, double vers, int bitVers, int deviceNum, String ip, double bal){
       super(m,uname,vers,bitVers,deviceNum,ip);
       currentBal = bal;
    }
+   //Returns the balance of the user
    public double getBal(){
       return currentBal;
    }
+   //Sets the balance of the user
    public void setBal(double bal){
       currentBal = bal;
    }
-   /*
-updateVersion(): void
-boot(): String
-runFile(name : String): String
-virusCheck(): String
-accessPersonalData() : String
-
-   */
+   //Adds a certain number of files if you have the memory and the money to do so
    public String addFiles(int amt){
       if(status && currentBal >= 1.0){
          if((fileAmount + amt) * 8 > getMemAmount()) 
@@ -35,18 +38,18 @@ accessPersonalData() : String
       }
       return "System is broken.";
    }
-   
+   //Increases the version by 0.1 if you have more than $10
    public void updateVersion(){
       if(currentBal >= 10.0){
          version += 0.1;
       }
    }
-   
+   //Creates a welcome message
    public String boot(){
       status = true;
       return "Welcome " + username + " to Windows Thirteen " + version + " thank you for using our system.";
    }
-   
+   //Runs the file, and will always detect a virus in the file :)
    public String runFile(String name){
       if(status){
          if(fileAmount != 0){
@@ -56,7 +59,7 @@ accessPersonalData() : String
       }
       return "Computer is off.";
    }
-   
+   //Randomizes the amount of viruses on your system
    public String virusCheck(){
       if(status){
          if(getAntiVirus() != null && !getAntiVirus().equals("")){
@@ -66,6 +69,7 @@ accessPersonalData() : String
       }
       return "Computer is off.";
    }
+   //Returns a sarcastic message about windows thirteen
    public String accessPersonalData(){
       return "Windows Thirteen totally does not store every single piece of data related to you.";
    }
