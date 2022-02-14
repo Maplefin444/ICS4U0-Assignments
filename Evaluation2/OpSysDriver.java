f5f861d589ed4d65580016725589df190bfa5b34
James Huynh <81708250+Maplefin444@users.noreply.github.com>
2022-02-14 10:14:53 -0500

    Add files via upload

--- Evaluation2/OpSysDriver.java
+++ Evaluation2/OpSysDriver.java
@@ -5,12 +5,14 @@ public class OpSysDriver
    {
    
       Operating_System os = new Operating_System(32000, "Prady", 1.0, 32, 1, "20.184.17.216");
-   
+
       System.out.println("The memory of this Operating System at purchase is " + os.getMemAmount() + " mb.");  
       System.out.println("The bit version of this Operating System at purchase is " + os.getBitVersion() + ".");
       System.out.println("There are " + os.getUserAmount() + " user(s) using your Operating System.");
       System.out.println("The antivirus currently on this system at purchase is " + os.getAntiVirus() + ".");
       System.out.println("The ip address you are using this Operating System at is " + os.getIPAddress() + ".");
+      try{Thread.sleep(5000);} catch(Exception e){}
+      System.out.println();
       os.setMemAmount(64000);
       os.setBitVersion(64);
       os.setUserAmount(2);
@@ -22,16 +24,19 @@ public class OpSysDriver
       System.out.println("The antivirus currently on this system has been updated to " + os.getAntiVirus() + ".");
       System.out.println("The ip address you are using this Operating System at has somehow changed to " + os.getIPAddress() + ".");
       System.out.println(os.addFiles(4));
-      System.out.println(os.fileAmount);
       System.out.println("There are now " + os.fileAmount + " files on your system.");
+      try{Thread.sleep(5000);} catch(Exception e){}
+      System.out.println();
       os.changeUser("James");
       System.out.println("User has been changed to " + os.username + ".");
       os.updateVersion();
       System.out.println("The version of your Operating System is now " + os.version + ".");
+      try{Thread.sleep(5000);} catch(Exception e){}
+      System.out.println();
+      System.out.println("Booting up the system...");
       System.out.println(os.boot() + ".");
       System.out.println(os.runFile("Do Not Open"));
       System.out.println(os.virusCheck());
-      
-   }
+      }
 
 }
