81b230f29889dbe79a8206572e919f2f04f2c35a
James Huynh <81708250+Maplefin444@users.noreply.github.com>
2022-03-04 09:59:25 -0500

    updated rng to adapt, and added words

--- ArrayList Evaluation/PradyumnJamesSentenceGen.java
+++ ArrayList Evaluation/PradyumnJamesSentenceGen.java
@@ -6,7 +6,7 @@
  */
 
 // Import statements
-import java.util.ArrayList;
+import java.util.*;
 
 // The "PradyumnJamesSentenceGen" class.
 public class PradyumnJamesSentenceGen
@@ -18,7 +18,6 @@ public class PradyumnJamesSentenceGen
       
       ArrayList<String> articles = new ArrayList<String>(); // Array List for articles
       articles.add("a");
-      articles.add("an");
       articles.add("the");
       
       ArrayList<String> nouns = new ArrayList<String>(); // Array List for nouns
@@ -29,6 +28,13 @@ public class PradyumnJamesSentenceGen
       nouns.add("girl");
       nouns.add("car");
       nouns.add("bridge");
+      nouns.add("coal");
+      nouns.add("sheep");
+      nouns.add("linen");
+      nouns.add("bed");
+      nouns.add("horse");
+      nouns.add("cow");
+      nouns.add("money");
       
       ArrayList<String> verbs = new ArrayList<String>(); // Array List for verbs
       verbs.add("jump");
@@ -36,6 +42,10 @@ public class PradyumnJamesSentenceGen
       verbs.add("fly");
       verbs.add("sleep");
       verbs.add("walk");
+      verbs.add("talk");
+      verbs.add("dance");
+      verbs.add("eat");
+      verbs.add("evade");
       
       ArrayList<String> prepositions = new ArrayList<String>(); // Array List for prepositions
       prepositions.add("at");
@@ -94,26 +104,59 @@ public class PradyumnJamesSentenceGen
       adverbs.add("merrily");
       adverbs.add("unnecessarily");
       adverbs.add("boldly");
+      adverbs.add("amazingly");
+      adverbs.add("heavily");
+      adverbs.add("unnaturally");
+      adverbs.add("occasionally");
       
       ArrayList<String> adjectives = new ArrayList<String>(); // Array List for adjectives
       adjectives.add("beautiful");
       adjectives.add("merciful");
       adjectives.add("grown");
+      adjectives.add("breakable");
+      adjectives.add("thick");
+      adjectives.add("young");
+      adjectives.add("tan");
+      adjectives.add("enchanted");
+      adjectives.add("groovy");
+      adjectives.add("unequaled");
+      adjectives.add("unrivaled");
+      adjectives.add("bizarre");
+      adjectives.add("royal");
+      adjectives.add("blunt");
+      adjectives.add("smart");
+      adjectives.add("dull");
+      adjectives.add("explosive");
+      adjectives.add("energetic");
+      adjectives.add("violent");
       
       // For-loop to print five randomly generated sentences
+      // The random methods were retrieved from https://docs.oracle.com/javase/8/docs/api/java/util/Random.html
+      // Random is used along with collections for even better random generation
+      Random a = new Random();
       for(int i = 0; i < 5; i++)
       {
-         String bword = articles.get((int)(Math.random() * 3));
+         a = new Random();
+         Collections.shuffle(articles);
+         String bword = articles.get(a.nextInt(articles.size()));
          bword = bword.substring(0,1).toUpperCase() + bword.substring(1);
          System.out.print(bword + " ");
-         System.out.print(adjectives.get((int)(Math.random() * 3)) + " ");
-         System.out.print(nouns.get((int)(Math.random() * 7)) + " ");
-         System.out.print(adverbs.get((int)(Math.random() * 5)) + " ");
-         System.out.print(verbs.get((int)(Math.random() * 5)) + " ");
-         System.out.print(prepositions.get((int)(Math.random() * 49)) + " ");
-         System.out.print(articles.get((int)(Math.random() * 3)) + " ");
-         System.out.print(adjectives.get((int)(Math.random() * 3)) + " ");
-         System.out.println(nouns.get((int)(Math.random() * 7)) + ".");
+         Collections.shuffle(adjectives);
+         System.out.print(adjectives.get(a.nextInt(adjectives.size())) + " ");
+         Collections.shuffle(nouns);
+         System.out.print(nouns.get(a.nextInt(nouns.size())) + " ");
+         Collections.shuffle(adverbs);
+         System.out.print(adverbs.get(a.nextInt(adverbs.size())) + " ");
+         Collections.shuffle(verbs);
+         System.out.print(verbs.get(a.nextInt(verbs.size())) + " ");
+         Collections.shuffle(prepositions);
+         System.out.print(prepositions.get(a.nextInt(prepositions.size())) + " ");
+         Collections.shuffle(articles);
+         System.out.print(articles.get(a.nextInt(articles.size())) + " ");
+         Collections.shuffle(adjectives);
+         System.out.print(adjectives.get(a.nextInt(adjectives.size())) + " ");
+         Collections.shuffle(nouns);
+         System.out.println(nouns.get(a.nextInt(nouns.size())) + ".");
       }
    }
 } // PradyumnJamesSentenceGen class
