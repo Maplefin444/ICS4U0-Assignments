abcd079a8446f7a2bc3839077d8fcb1eb7c37197
pjha4 <pradyumnjha2006@gmail.com>
2022-03-03 19:38:57 -0500

    Finished Sentence Generation Program

---
+++ ArrayList Evaluation/SentenceGenDriver.java
@@ -0,0 +1,19 @@
+/*
+   Programmers: Pradyumn Jha and James Huynh
+   Teacher: Ms. Krasteva
+   Date: March 2nd, 2022 (2022-03-02)
+   Purpose: This program will run the sentence generating program
+ */
+
+// The "SentenceGenDriver" class.
+public class SentenceGenDriver
+{
+
+   // The main method responsible for executing other methods
+   public static void main (String[] args)
+   {
+   
+      PradyumnJamesSentenceGen sg = new PradyumnJamesSentenceGen();
+      sg.sentenceGenerator();  
+   }
+} // SentenceGenDriver class
