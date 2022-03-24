159069e73e6cd22019502756f0a90d41162bcd0c
Daniel Ye <danielyeqt@gmail.com>
2022-03-23 20:09:45 -0400

    Comment StudentSortingDriver.java

--- Sorting/StudentSortingDriver.java
+++ Sorting/StudentSortingDriver.java
@@ -1,9 +1,24 @@
+/*
+* Programmers: James Huynh, Pradyumn Jha, Daniel Ye
+* Date: March 23, 2022
+* Teacher: Ms. Krasteva
+* Description: The Driver for the StudentSorting program, in which the students described in A7-1.txt are sorted and printed out.
+*/
+
+/**
+* StudentSortingDriver displays the students described in A7-1.txt in sorted order to the console, both by name and by mark
+*/
 public class StudentSortingDriver{
    public static void main(String[] args){
+      // declare StudentSorting
       StudentSorting a = new StudentSorting();
+      
+      // print out students sorted by name (increasing lexicographic)
       System.out.println("---------- SORTED BY NAME --------------------");
       a.sortNames();
       a.printStudents();
+      
+      // print out students sorted by mark (descending)
       System.out.println("---------- SORTED BY MARK --------------------");
       a.sortByMark();
       a.printStudents();
