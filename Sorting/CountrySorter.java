d9d32a4f46ea330bdcbeecc0760b003e17a94e9d
James Huynh <81708250+Maplefin444@users.noreply.github.com>
2022-03-25 09:39:42 -0400

    loggers

--- Sorting/CountrySorter.java
+++ Sorting/CountrySorter.java
@@ -11,20 +11,77 @@ public class CountrySorter{
    /**
    * The name of the country
    */
-   private ArrayList<String> countryName;
+   private ArrayList<String> countryName = new ArrayList<String>();
    /**
    * The name of the capital
    */
-   private ArrayList<String> capital;
+   private ArrayList<String> capital = new ArrayList<String>();
    /**
    * the land area of the country
    */
-   private ArrayList<Integer> area;
+   private ArrayList<Double> area = new ArrayList<Double>();
    /**
    * The population of the country
    */
-   private ArrayList<Integer> population;
+   private ArrayList<Integer> population = new ArrayList<Integer>();
    
+   public static void main(String[] args){
+      CountrySorter a = new CountrySorter();
+      a.readFile();
+      a.sortByPopulation();
+      a.printToFile("loggers.txt");
+   }
+   public void readFile(){
+      int[] lines = {50,53,54,57,67,68,76,89,90,91,105,109,111,112,116,120,125,133, 3, 5, 6, 15, 20, 21,24,26,28,31,32,33,37,38,39,40,41,45,49, 134, 144, 145, 146, 148, 149, 150, 154, 158, 160, 162, 175, 181, 182, 183, 184, 188, 191};
+      int[] wordamt = {2,2,2,1,1,1,1,2,2,1,1,2,1,1,1,2,2,1,1,3,1,1,1,3,1,2,1,2,3,1,1,3,5,2,2,2,2, 3, 4, 2, 5, 2, 4, 2, 2, 2, 2, 2, 3, 1, 2, 2, 2, 2, 2};
+      try{
+         BufferedReader br = new BufferedReader(new FileReader("Countries-Population.txt"));
+         int count = 0;
+         while(true){
+            String a = br.readLine();
+            if(a == null) 
+               break;
+            String[] line = a.split(" ");
+            if(line.length == 4){
+               countryName.add(line[0]);
+               capital.add(line[1]);
+               area.add(Double.parseDouble(line[2].replaceAll(",","")));
+               population.add(Integer.parseInt(line[3].replaceAll(",","")));
+            }
+            for(int i = 0 ; i < lines.length; i++){
+               if(count == lines[i]){
+                  String cname = "";
+                  for(int j = 0; j < wordamt[i];j++){
+                     if(j == wordamt[i]-1){
+                        cname += line[j];
+                     }
+                     else{
+                        cname += line[j] + " ";
+                     }
+                  }
+                  String capname = "";
+                  for(int j = 0 ; j < line.length - wordamt[i] - 2;j++){
+                     if(j == line.length - wordamt[i] - 3){
+                        capname += line[j+wordamt[i]];
+                     }
+                     else{
+                        capname += line[j+wordamt[i]] + " ";
+                     }
+                  }
+                  
+                  countryName.add(cname.trim());
+                  capital.add(capname.trim());
+                  area.add(Double.parseDouble(line[line.length-2].replaceAll(",","")));
+                  population.add(Integer.parseInt(line[line.length-1].replaceAll(",","")));
+               }
+            }
+            count++;
+         }
+      }
+      catch(Exception e){
+         e.printStackTrace();
+      }
+   }
    /**
    * This method internally sorts the 4 parallel arraylists for the list of countries using a mergesort algorithm
    * @param l this is used for the recursive mergesort - the left bound of the segment this method call will sort
@@ -32,12 +89,14 @@ public class CountrySorter{
    */
    private void sortByPopulation(int l, int r){
       // if the length of the segment is 1, then no changes are needed
-      if(l == r) return;
+      if(l == r) 
+         return;
+      
       
       // temporary arrays for the country, capital, area, and population, which will later be copied into the original
       ArrayList<String> countTmp = new ArrayList<String>();
       ArrayList<String> capTmp = new ArrayList<String>();
-      ArrayList<Integer> areaTmp = new ArrayList<Integer>();
+      ArrayList<Double> areaTmp = new ArrayList<Double>();
       ArrayList<Integer> popTmp = new ArrayList<Integer>();
       
       // calculate the middle and recursively sort both halves (instance variables are updated)
