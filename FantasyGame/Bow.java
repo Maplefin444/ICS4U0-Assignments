f1eb6f7b28e5c6d57ae22d3308d934313cbacc17
Daniel Ye <danielyeqt@gmail.com>
2022-04-18 09:55:44 -0400

    Add Bow

---
+++ FantasyGame/Bow.java
@@ -0,0 +1,32 @@
+/*
+* James Huynh, Daniel Ye
+* April 18, 2022
+* Ms. Krasteva
+* This class implements a Bow in the fantasy game
+*/
+
+/**
+* An item in the game
+*/
+public class Bow implements Item{
+   /**
+   * The amount of damage dealt by this bow
+   */
+   int damage;
+   
+   /**
+   * Class Constructor for a bow
+   * @param the amount of damage dealt by the bow
+   */
+   public Bow(int damage){
+      this.damage = damage;
+   }
+
+   /**
+   * uses the item
+   * @return a String describing the action of the item
+   */
+   public String useItem(Player c){
+      return "Bow deals " + damage + " damage!";
+   }
+}
---
+++ FantasyGame/Sword.java
@@ -0,0 +1,32 @@
+/*
+* James Huynh, Daniel Ye
+* April 18, 2022
+* Ms. Krasteva
+* This class implements a sword in the fantasy game
+*/
+
+/**
+* An item in the game
+*/
+public class Sword implements Item{
+   /**
+   * The amount of damage dealt by this sword
+   */
+   int damage;
+   
+   /**
+   * Class Constructor for a sword
+   * @param the amount of damage dealt by the sword
+   */
+   public Sword(int damage){
+      this.damage = damage;
+   }
+
+   /**
+   * uses the item
+   * @return a String describing the action of the item
+   */
+   public String useItem(Player c){
+      return "Sword deals " + damage + " damage!";
+   }
+}
