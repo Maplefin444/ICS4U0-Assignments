29b7c6674569225b51562abda8caa6e3e32bbdb2
Maplefin <james.huynh444@gmail.com>
2022-04-30 19:58:20 -0400

    evelans :weary:

--- Elevans Lab/Elevans Lab Activity Starter Code/Activity9  Starter Code/ElevensBoard.java
+++ Elevans Lab/Elevans Lab Activity Starter Code/Activity9  Starter Code/ElevensBoard.java
@@ -9,38 +9,38 @@ public class ElevensBoard extends Board {
 	/**
 	 * The size (number of cards) on the board.
 	 */
-	private static final int BOARD_SIZE = 9;
+   private static final int BOARD_SIZE = 9;
 
 	/**
 	 * The ranks of the cards for this game to be sent to the deck.
 	 */
-	private static final String[] RANKS =
-		{"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
+   private static final String[] RANKS =
+   	{"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"};
 
 	/**
 	 * The suits of the cards for this game to be sent to the deck.
 	 */
-	private static final String[] SUITS =
-		{"spades", "hearts", "diamonds", "clubs"};
+   private static final String[] SUITS =
+   	{"spades", "hearts", "diamonds", "clubs"};
 
 	/**
 	 * The values of the cards for this game to be sent to the deck.
 	 */
-	private static final int[] POINT_VALUES =
-		{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0};
+   private static final int[] POINT_VALUES =
+   	{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 0, 0};
 
 	/**
 	 * Flag used to control debugging print statements.
 	 */
-	private static final boolean I_AM_DEBUGGING = false;
+   private static final boolean I_AM_DEBUGGING = false;
 
 
 	/**
 	 * Creates a new <code>ElevensBoard</code> instance.
 	 */
-	 public ElevensBoard() {
-	 	super(BOARD_SIZE, RANKS, SUITS, POINT_VALUES);
-	 }
+   public ElevensBoard() {
+      super(BOARD_SIZE, RANKS, SUITS, POINT_VALUES);
+   }
 
 	/**
 	 * Determines if the selected cards form a valid group for removal.
@@ -51,10 +51,15 @@ public class ElevensBoard extends Board {
 	 * @return true if the selected cards form a valid group for removal;
 	 *         false otherwise.
 	 */
-	@Override
-	public boolean isLegal(List<Integer> selectedCards) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
-	}
+    //-------------------------------------------------------------------------implemented these methods
+   @Override
+   public boolean isLegal(List<Integer> selectedCards) {
+      if (selectedCards.size() == 2) 
+         return containsPairSum11(selectedCards);
+      else if (selectedCards.size() == 3) 
+         return containsJQK(selectedCards);
+      return false;
+   }
 
 	/**
 	 * Determine if there are any legal plays left on the board.
@@ -64,10 +69,11 @@ public class ElevensBoard extends Board {
 	 * @return true if there is a legal play left on the board;
 	 *         false otherwise.
 	 */
-	@Override
-	public boolean anotherPlayIsPossible() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
-	}
+   @Override
+   public boolean anotherPlayIsPossible() {
+      List<Integer> arr = cardIndexes();
+      return containsPairSum11(arr) || containsJQK(arr);
+   }
 
 	/**
 	 * Check for an 11-pair in the selected cards.
@@ -77,9 +83,17 @@ public class ElevensBoard extends Board {
 	 * @return true if the board entries in selectedCards
 	 *              contain an 11-pair; false otherwise.
 	 */
-	private boolean containsPairSum11(List<Integer> selectedCards) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
-	}
+   private boolean containsPairSum11(List<Integer> selectedCards) {
+      if(selectedCards.size() < 2) 
+         return false;
+      for(int i = 0; i < selectedCards.size();i++){
+         for(int j = i+1; j < selectedCards.size();j++){
+            if(cardAt(selectedCards.get(i)).pointValue() + cardAt(selectedCards.get(j)).pointValue() == 11) 
+               return true;
+         }
+      }
+      return false;
+   }
 
 	/**
 	 * Check for a JQK in the selected cards.
@@ -89,7 +103,18 @@ public class ElevensBoard extends Board {
 	 * @return true if the board entries in selectedCards
 	 *              include a jack, a queen, and a king; false otherwise.
 	 */
-	private boolean containsJQK(List<Integer> selectedCards) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 9 *** */
-	}
+   private boolean containsJQK(List<Integer> selectedCards) {
+      if(selectedCards.size() < 3) 
+         return false;
+      boolean a=false,b=false,c = false;
+      for(int i = 0; i < selectedCards.size();i++){
+         if (cardAt(selectedCards.get(i)).rank() == "jack") a = true;
+         else if (cardAt(selectedCards.get(i)).rank() == "queen") b = true;
+         else if (cardAt(selectedCards.get(i)).rank() == "king") c = true;
+         if(a && b && c) return true;
+      }
+      return false;
+   }
+   //--------------------------------------------------------------------method implementation ends here
+   
 }
